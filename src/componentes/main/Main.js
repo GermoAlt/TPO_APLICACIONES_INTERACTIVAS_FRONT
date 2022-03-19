fa9f6e42be26eea20c947dc03b499840fc4ae910
Tobías Kleppe <tkleppe@uade.edu.ar>
2022-03-19 15:11:16 -0300

    WIP: Profile + myRecipes display setup

--- src/componentes/main/Main.js
+++ src/componentes/main/Main.js
@@ -1,11 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./home/Home";
+import Profile from "./perfil/Profile";
 
 export default function Main(){
     return (
         <div>
             <Routes>
                 <Route exact path={"/"} element={<Home/>}/>
+                <Route exact path={"/profile"} element={<Profile/>}/>
             </Routes>
         </div>
     )
