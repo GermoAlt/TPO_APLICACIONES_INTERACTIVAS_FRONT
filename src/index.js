fdc4b8c20fdf1c71d796ba7653e2155eda72b5e0
German <germanaltairac@hotmail.com>
2022-03-19 12:32:15 -0300

    tema

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '/node_modules/primeflex/primeflex.css';
+import 'primereact/resources/themes/saga-orange/theme.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
