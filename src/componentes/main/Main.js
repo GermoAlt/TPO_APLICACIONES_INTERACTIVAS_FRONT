3b2c57584e91e5b72f55679a5d4118a6a5f18119
German <germanaltairac@hotmail.com>
2022-03-22 17:45:25 -0300

    WIP info receta

--- src/componentes/main/Main.js
+++ src/componentes/main/Main.js
@@ -1,11 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./home/Home";
+import InfoReceta from "./recetas/InfoReceta";
 
 export default function Main(){
     return (
         <div>
             <Routes>
                 <Route exact path={"/"} element={<Home/>}/>
+                <Route path={"/receta/:id"} element={<InfoReceta/>}/>
             </Routes>
         </div>
     )
--- src/componentes/main/recetas/InfoReceta.js
+++ src/componentes/main/recetas/InfoReceta.js
@@ -1,7 +1,24 @@
-export default function InfoReceta() {
-    return (
-        <div>
+import dataReceta from '../../../api/recetas.json'
+import {useParams} from "react-router-dom";
 
+export default function InfoReceta() {
+    const navigate = useParams()
+    const receta = dataReceta.find(item => String(item.id) === navigate.id)
+    console.log(receta)
+    return (
+        <div className={"info-receta"}>
+            <div className={"info-receta-titulo"}>
+                {receta.titulo}
+            </div>
+            <div className={"info-receta-descripcion"}>
+                {receta.descripcion}
+            </div>
+            <div className={"info-receta-ingredientes"}>
+
+            </div>
+            <div className={"info-receta-pasos"}>
+
+            </div>
         </div>
     )
 }
