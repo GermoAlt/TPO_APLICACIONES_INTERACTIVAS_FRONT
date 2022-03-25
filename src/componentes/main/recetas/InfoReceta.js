160c699513aecbdceb9e75650c1e358cd5dd8285
German <germanaltairac@hotmail.com>
2022-03-25 20:57:21 -0300

    error handling

--- src/componentes/main/recetas/InfoReceta.js
+++ src/componentes/main/recetas/InfoReceta.js
@@ -7,7 +7,9 @@ import {fill} from "@cloudinary/url-gen/actions/resize";
 import {byRadius} from "@cloudinary/url-gen/actions/roundCorners";
 import {useState} from "react";
 
+
 export default function InfoReceta() {
+    const [errorClass, setErrorClass] = useState("")
     const navigate = useParams()
     const receta = dataReceta.find(item => String(item.id) === navigate.id)
     return (
@@ -15,21 +17,24 @@ export default function InfoReceta() {
             <div className={"info-receta-container-card info-receta-titulo"}>
                 {receta.titulo}
             </div>
-            {receta.imagen ?
-                <div className={"info-receta-container-card info-receta-imagen"}>
-                    <AdvancedImage cldImg={getImagen("receta/"+receta.imagen).roundCorners(byRadius(25))} plugins={[responsive({steps:1})]}/>
-                </div>
-                :null
-            }
+            <div className={`info-receta-container-card info-receta-imagen ${errorClass}`}>
+                <AdvancedImage cldImg={getImagen("receta/"+receta.imagenes[0]).roundCorners(byRadius(25))}
+                               plugins={[responsive({steps:1})]} onError={(e) => {
+                                   setErrorClass("image-not-found")
+                                   mostrarError(e)
+                }}/>
+            </div>
             <div className={"info-receta-container-card info-receta-descripcion"}>
                 {receta.descripcion}
             </div>
             <div className={"info-receta-container-card info-receta-ingredientes"}>
+                <h1>Ingredientes</h1>
                 <ul>
                     {buildIngredients(receta.ingredientes)}
                 </ul>
             </div>
             <div className={"info-receta-container-card info-receta-pasos"}>
+                <h1>Pasos</h1>
                 <ol>
                     {buildSteps(receta.pasos)}
                 </ol>
@@ -44,6 +49,10 @@ function buildIngredients(ingredientes){
             <b>{ingrediente.cantidad}</b> {ingrediente.ingrediente}
         </li>
     ))
+}
+
+function mostrarError(e){
+    e.target.src="https://png.pngtree.com/png-vector/20190917/ourmid/pngtree-not-found-line-icon-vectors-png-image_1737850.jpg"
 }
 
 function buildSteps(pasos){
