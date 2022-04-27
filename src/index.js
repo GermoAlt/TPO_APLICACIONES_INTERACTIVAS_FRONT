3dca3ba8b223094ee5737ba1e8d25e48afe106d0
Germán Altairac <germanaltairac@hotmail.com>
2022-04-27 19:39:44 -0300

    remove dependency

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import '/node_modules/primeflex/primeflex.css';
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import 'primereact/resources/themes/saga-orange/theme.css';
-import 'animate.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
