ccb5a7c0c626059f331a45dfcdcb5f0e40fc873c
Germán Altairac <germanaltairac@hotmail.com>
2022-04-27 21:31:04 -0300

    banner

--- src/componentes/main/home/Home.js
+++ src/componentes/main/home/Home.js
@@ -3,10 +3,10 @@ import Banner from "./banner/Banner";
 export default function Home() {
     return (
         <div>
-            <Banner isGuest={true}/>
-        <h1>
-            si lees esto, el proyecto levantó :)
-        </h1>
+            <Banner isGuest={false}/>
+            <h1>
+                si lees esto, el proyecto levantó :)
+            </h1>
             <h2>
                 este texto esta en la home (Home.js)
             </h2>
