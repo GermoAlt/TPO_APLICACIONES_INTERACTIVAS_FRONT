a2cba4a0b6d1e8e508905556cba41223abda9856
Belen <elbe.enriquez@gmail.com>
2022-05-14 00:36:11 -0300

    Modal edit profile y cambio msj de confirmacion alta

--- src/componentes/main/perfil/Profile.js
+++ src/componentes/main/perfil/Profile.js
@@ -7,12 +7,45 @@ import {thumbnail} from "@cloudinary/url-gen/actions/resize";
 import {byRadius} from "@cloudinary/url-gen/actions/roundCorners";
 import {focusOn} from "@cloudinary/url-gen/qualifiers/gravity";
 import {FocusOn} from "@cloudinary/url-gen/qualifiers/focusOn";
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
 
 import './profile.css';
 import profileData from './profile.json';
 import RecipeList from '../busqueda/RecipeList';
+import { Button } from 'primereact/button';
 
 const Profile = () => {
+    const [displayResponsive, setDisplayResponsive] = useState(false);
+    const [position, setPosition] = useState('center');
+    const [value1, setValue1] = useState('');
+    const [value2, setValue2] = useState('');
+
+    const onHide = (name) => {
+        dialogFuncMap[`${name}`](false);
+    }
+
+    const renderFooter = (name) => {
+        return (
+            <div>
+                <Button label="Cancelar" icon="pi pi-times" onClick={() => onHide(name)} className="p-button-text" />
+                <Button label="Guardar" icon="pi pi-check" onClick={() => onHide(name)} autoFocus />
+            </div>
+        );
+    }
+    const dialogFuncMap = {
+        'displayResponsive': setDisplayResponsive
+    }
+
+    const onClick = (name, position) => {
+        dialogFuncMap[`${name}`](true);
+
+        if (position) {
+            setPosition(position);
+        }
+    }
+    
     const [user, setUser] = useState(profileData);
     const image = getImagen(user.imageId).resize(thumbnail().width(250).height(250).gravity(focusOn(FocusOn.face()))).roundCorners(byRadius(150))
     useEffect(() => {
@@ -29,14 +62,32 @@ const Profile = () => {
                                 </div>
                             </div>
                             <div className="col-8 flex flex-wrap align-items-center">
+                                <Button type="button" icon='pi pi-pencil' onClick={() => onClick('displayResponsive')}/>
                                 <div className='grid flex flex-wrap align-items-center'>
-                                    <div className="col-12 profile-name">
-                                        {user.name}
-                                    </div>
-                                    <div className="col-12 profile-description">
-                                        {user.description}
-                                    </div>
+                                        <div className="col-12 profile-name">
+                                            {user.name}
+                                        </div>
+                                        <div className="col-12 profile-description">
+                                            {user.description}
+                                        </div>
+                                    
                                 </div>
+                            </div>
+                            <div className='align-items-center'>
+                                    <Dialog header="Editar perfil" visible={displayResponsive} onHide={() => onHide('displayResponsive')} breakpoints={{'960px': '75vw'}} style={{width: '50vw'}} footer={renderFooter('displayResponsive')}>
+                                            <div className="field">
+                                                <label>Nombre</label>
+                                                <InputText id="name" value={user.name} />
+                                            </div>
+                                            <div className="">
+                                                <label>Nueva password</label>
+                                                <Password value={value1} onChange={(e) => setValue1(e.target.value)} toggleMask />
+                                            </div>
+                                            <div className="field">
+                                                <label>Confirmar password</label>
+                                                <Password value={value2} onChange={(e) => setValue2(e.target.value)} toggleMask feedback={false}/>
+                                            </div>
+                                    </Dialog>
                             </div>
                         </div>: 
                         <div>
---
+++ src/componentes/main/recetas/EditReceta.js
@@ -0,0 +1,168 @@
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+
+import dataReceta from '../../../api/recetas.json'
+import dataReviews from '../../../api/calificaciones.json'
+import {useParams} from "react-router-dom";
+import "./infoReceta.css"
+import {AdvancedImage, responsive} from "@cloudinary/react";
+import {getImagen} from "../../imagen/getImagenCloud";
+import {byRadius} from "@cloudinary/url-gen/actions/roundCorners";
+import {useState} from "react";
+import {Carousel} from "primereact/carousel";
+import {Rating} from "primereact/rating";
+import {Tag} from "primereact/tag";
+import {Button} from "primereact/button";
+import {Editor} from "primereact/editor";
+import { InputText } from 'primereact/inputtext';
+
+
+export default function EditReceta() {
+    const [errorClass, setErrorClass] = useState("")
+    const [mostrarEditor, setMostrarEditor] = useState(false)
+    const [newRatingText, setNewRatingText] = useState("")
+    const [newRatingValue, setNewRatingValue] = useState(0)
+    const [reviews, setReviews] = useState([...dataReviews]);
+    const navigate = useParams()
+    const receta = dataReceta.find(item => String(item.id) === navigate.id)
+    const [edit, setEdit] = useState(dataReceta);
+
+    const imagenTemplate = (imagen) => {
+        return(
+            <div className={`info-receta-imagen${errorClass}`}>
+                <AdvancedImage cldImg={getImagen("receta/"+imagen).roundCorners(byRadius(25))}
+                               plugins={[responsive({steps:1})]} onError={(e) => {
+                    setErrorClass(" image-not-found")
+                    mostrarError(e)
+                }}/>
+            </div>
+        )
+    }
+
+    const renderHeader = () => {
+        return (
+            <span className="ql-formats">
+                <button className="ql-bold" aria-label="Bold"/>
+                <button className="ql-italic" aria-label="Italic"/>
+                <button className="ql-underline" aria-label="Underline"/>
+            </span>
+        );
+    }
+
+    const header = renderHeader();
+    const [data, setData] = useState({receta});
+    
+    const handleChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value});
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className={"info-receta-container"}>
+                <div className={"new-receta-details-item"}>
+                    <Editor value={receta.titulo} onChange={handleChange}></Editor>
+                </div>
+                <div className={"gourmetic-card info-receta-descripcion"}>
+                    <InputText value={receta.descripcion} onChange={handleChange}></InputText>
+                </div>
+                <div className={"info-receta-central-panel"}>
+                    {
+                        receta.categorias.length !== 1 ?
+                            <div className={`info-receta-imagen ${errorClass}`}>
+                                <AdvancedImage cldImg={getImagen("receta/"+receta.imagenes[0])}
+                                            plugins={[responsive({steps:1})]} onError={(e) => {
+                                    setErrorClass("image-not-found")
+                                    mostrarError(e)
+                                }}/>
+                            </div>
+                        :
+                        <div className={"gourmetic-card info-receta-carousel-container"}>
+                            <Carousel  className={"info-receta-carousel"} value={receta.imagenes} numVisible={1} numScroll={1} itemTemplate={imagenTemplate}/>
+                        </div>
+                    }
+
+                    <div className={"gourmetic-card info-receta-details"}>
+                        <div className={"info-receta-details-item"}>
+                            <span>Dificultad</span>
+                            <Rating value={receta.dificultad} readOnly stars={5} cancel={false} disabled className={"override-opacity"}/>
+                        </div>
+                        <div className={"info-receta-details-item"}>
+                            <span>Preparación</span>
+                            <b>{buildTime(receta.tiempoPreparacion)}</b>
+                        </div>
+                        <div className={"info-receta-details-item"}>
+                            <span>Elaboración</span>
+                            <b>{buildTime(receta.tiempoElaboracion)}</b>
+                        </div>
+                        <div className={"info-receta-details-item"}>
+                            <span>Categorias</span>
+                            <div className={"info-receta-categorias"}>
+                                {buildCategories(receta.categorias)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={"info-receta-central-panel"}>
+                    <div className={"gourmetic-card info-receta-ingredientes"}>
+                        <h3>Ingredientes</h3>
+                        <ul className={"info-receta-list"}>
+                            {buildIngredients(receta.ingredientes)}
+                        </ul>
+                    </div>
+                    <div className={"gourmetic-card info-receta-pasos"}>
+                        <h3>Pasos</h3>
+                        <ol className={"info-receta-list"}>
+                            {buildSteps(receta.pasos)}
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+
+
+function buildTime(tiempoEnMinutos){
+    const hours = Math.floor(tiempoEnMinutos / 60);
+    const minutes = tiempoEnMinutos % 60;
+    let res = ""
+    if (hours !== 0){
+        res += hours + " hr" + (hours > 1 ? "s " : " ")
+    }
+    if (minutes !== 0) {
+        res += minutes + " min"
+    }
+    return res
+}
+
+function buildCategories(categorias) {
+    return categorias.map(categoria => (
+        <Tag key={categoria} value={categoria} rounded />
+    ))
+}
+
+function buildIngredients(ingredientes){
+    return ingredientes.map(ingrediente => (
+        <li key={ingrediente}>
+            <b>{ingrediente.cantidad}</b> {ingrediente.ingrediente}
+        </li>
+    ))
+}
+
+function mostrarError(e){
+    e.target.src="https://png.pngtree.com/png-vector/20190917/ourmid/pngtree-not-found-line-icon-vectors-png-image_1737850.jpg"
+}
+
+function buildSteps(pasos){
+    return pasos.map(paso => (
+        <li key={paso.orden}>
+            {paso.paso}
+        </li>
+    ))
+}
+
+
